28d441cd54369ab062d7f0fc49f8ff83e8dbea92
Arturo <38075@ma-web.nl>
2024-06-06 15:01:59 +0200

    02 huisdierselct

---
+++ 01 opstarten/dockerM4Prog/sql/02 data/02 huisdierselect.sql
@@ -0,0 +1,7 @@
+SELECT naam FROM `huisdieren`;
+SELECT * FROM `huisdieren`;
+
+SELECT naam, eigenaar FROM `huisdieren`;
+SELECT eigenaar, naam FROM `huisdieren`;
+
+SELECT id, typedier, leeftijd, geboortedatum FROM `huisdieren`;
