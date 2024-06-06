4728cd64ba01c253d7e499629463819953688686
Arturo <38075@ma-web.nl>
2024-06-06 14:48:18 +0200

    les 2 01 data

---
+++ 01 opstarten/dockerM4Prog/sql/01 opstarten/01 huidierendata.sql
@@ -0,0 +1,7 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+    (NULL, 'Benson', 'John', 'Konijn', '3', '2021-05-05'),
+    (NULL, 'Kamut', 'Jingo', 'Cavia', '4', '2021-05-05'),
+    (NULL, 'Lamo', 'David', 'Hond', '3', '2021-05-05'),
+    (NULL, 'Baku', 'Damian', 'Vis', '2', '2022-05-05'),
+    (NULL, 'Woofer', 'Jaap', 'Hond', '12', '2014-02-21');
---
+++ 01 opstarten/dockerM4Prog/sql/02 data/huisdierendata.sql
@@ -0,0 +1,2 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) VALUES (NULL, 'Lamo', 'David', 'Hond', '3', '2021-05-05');
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) VALUES (NULL, 'Baku', 'Damian', 'Vis', '2', '2022-05-05');
